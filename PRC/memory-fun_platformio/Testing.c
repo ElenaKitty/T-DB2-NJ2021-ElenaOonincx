c6dc9ab218223cc641d2bd7ac41fcd5cd5e1bd1f
Elena <potatokitty98@gmail.com>
2020-09-10 14:52:56 +0200

    Pointer excersises

--- PRC/memory-fun_platformio/Testing.c
+++ PRC/memory-fun_platformio/Testing.c
@@ -1,16 +1,56 @@
 #include <stdio.h>
+void metricToImperial(int lengthInCm, int* feet, int* remainingInches)
+{
+    *feet = lengthInCm / 30.48;
+    *remainingInches = (lengthInCm - *feet * 30.48) / 2.54;
+}
+void swapValues(int* value1, int* value2)
+{
+    int temp = *value1;
+    *value1 = *value2;
+    *value2 = temp; 
+}
+
 int main(int argc, char *argv[])
 {
-    /* define a local variable a */
-int a = 1;
-char b = 2;
 
-/* define a pointer variable, and point it to a using the & operator */
-int * pointer_to_a = &a;
-char * pointer_to_b = &b;
+    //memory fun
+    int a = 1;
+    char b = 2;
+    int * pointer_to_a = &a;
+    char * pointer_to_b = &b;
 
-printf("The value a is %d\n", a);
-printf("The value b is %d\n", b);
-printf("The value of a is also %d\n", *pointer_to_a);
-printf("The value of b is also %d\n", *pointer_to_b);
+    printf("The value a is %d\n", a);
+    printf("The value b is %d\n", b);
+    printf("The value of a is also %d\n", *pointer_to_a);
+    printf("The value of b is also %d\n", *pointer_to_b);
+
+    //assignment 2.2 change a pointer value
+    int number = 0;
+    int * writePointer = &number;
+    int * readPointer = &number;
+
+    printf("number before write is %d\n", *readPointer);
+    *writePointer = 10; 
+    printf("number after write is %d\n", *readPointer);
+
+    int lengthInCm = 163;
+    int feet = 0;
+    int remmainingInches = 0;
+    int * feetPointer = &feet;
+    int * inchPointer = &remmainingInches;
+
+    //assignment 2.3 Metric to Imperial conversion
+    metricToImperial(lengthInCm, feetPointer, inchPointer);
+    printf("Length in cm: %d --> Feet: %d Inches: %d\n", lengthInCm, feet, remmainingInches);
+
+
+    //assignment 2.4 Value swap
+    int value1 = 30;
+    int value2 = 69;
+    int* pointerValue1 = &value1;
+    int* pointerValue2 = &value2;
+    printf("Value1: %d  and Value2: %d\n", value1, value2);
+    swapValues(pointerValue1, pointerValue2);
+    printf("Value1: %d  and Value2: %d\n", value1, value2);
 }
