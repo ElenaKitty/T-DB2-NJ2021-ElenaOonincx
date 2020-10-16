4ecdaf2a39f8314995507390ce75ccdcca680dd4
Elena <potatokitty98@gmail.com>
2020-10-16 15:18:21 +0200

    pointerExcersize

--- PRC/Pointers& Unit Tests/Uitwerking/main.c
+++ PRC/Pointers& Unit Tests/Uitwerking/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int passByValue(int value)
 {
@@ -7,22 +8,57 @@ int passByValue(int value)
     return value;
 }
 
-void passByRefernce(int* pointerValue)
+void passByRefernce(int *pointerValue)
 {
     *pointerValue = *pointerValue * *pointerValue * *pointerValue;
+}
+void printConstArray(const char *constArray, int arrayLength)
+{
+    for (int i = 0; i < arrayLength; i++)
+    {
+        printf("%c", constArray[i]);
+    }
 }
 
 int main(int argc, char *argv[])
 {
     //Oefening 1
+    printf("--------------------------\n");
+    printf("Pointer assignment\n");
+    printf("--------------------------\n");
     int value = 6;
     printf("The value is now %d\n", value);
     value = passByValue(value);
     printf("The value is now %d\n", value);
-    int * pointerValue = &value;
+    int *pointerValue = &value;
     passByRefernce(pointerValue);
     printf("The value is now %d\n", value);
 
-    //Oefening 2
-    
+    //Oefening 2m
+    printf("--------------------------\n");
+    printf("Capitalize assignment \n");
+    printf("--------------------------\n");
+    int arrayLength = 25;
+    char array[] = {"De hond loopt in het gras"};
+    for (int i = 0; i < arrayLength; i++)
+    {
+        printf("%c", array[i]);
+    }
+    for (int i = 0; i < arrayLength; i++)
+    {
+        array[i] = toupper(array[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < arrayLength; i++)
+    {
+        printf("%c", array[i]);
+  
+    }
+    printf("\n");
+    //Oefening 3
+    printf("--------------------------\n");
+    printf("Constant array assignment\n");
+    printf("--------------------------\n");
+    printConstArray(array, arrayLength);
+    printf("\n");
 }
